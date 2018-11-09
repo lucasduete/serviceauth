3def79741979a11871311f89cdfeb60bc81bb1d6
Lucas Duete <lm.duete@gmail.com>
2018-11-09 09:20:39 -0300

    First Commit

---
+++ src/main/java/io/github/lucasduete/pweb2/serviceauth/ServiceauthApplication.java
@@ -0,0 +1,12 @@
+package io.github.lucasduete.pweb2.serviceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceauthApplication.class, args);
+	}
+}
